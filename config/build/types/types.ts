8038f4b2a8b3c10f86f1d6acf78753347b0d5ddc
vilronn <102553486+vilronn@users.noreply.github.com>
2024-09-25 08:58:13 +0300

    Update types.ts

--- config/build/types/types.ts
+++ config/build/types/types.ts
@@ -1,13 +1,15 @@
 export interface BuildPaths {
-    entry: string,
-    html: string,
-    output: string,
-}
+    entry: string;
+    html: string;
+    output: string;
+    src: string;
+    }
 
 export type BuildMode = 'production' | 'development';
 
 export interface BuildOptions {
-    port: number,
-    paths: BuildPaths,
-    mode: BuildMode,
+    port: number;
+    paths: BuildPaths;
+    mode: BuildMode;
+    analyzer?: boolean;
 }
