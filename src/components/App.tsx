6cda631e66792b3bdc1e24944dd840442b82c434
vilronn <102553486+vilronn@users.noreply.github.com>
2024-09-25 08:53:21 +0300

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,10 @@
 import React, {useState} from 'react';
 import classes from './App.module.scss';
+import {Link, Outlet } from 'react-router-dom';
+import About from "@/pages/about/About";
+import duckPng from "@/assets/duck.png";
+import fallJpg from "@/assets/fall.jpg";
+import LeaveSvg from "@/assets/leave.svg";
 
 export const App = () => {
     const [count, setCount] = useState<number>(0);
@@ -7,8 +12,21 @@ export const App = () => {
 
     return (
     <div>
-        <h1>{count}</h1>
-        <button className={classes.button} onClick={increment}>BUTTON</button>
+        <div>
+            <img width={200} src={fallJpg} alt="fall" />
+            <img width={50} src={duckPng} alt="duck" />
+            
         </div>
+        <div>
+            <LeaveSvg width={200} fill='red'/>
+        </div>
+        <Link to={'/about'}>about</Link>
+        <br/>
+        <Link to={'/shop'}>shop</Link>
+        <h1 className={classes.value}>{count}</h1>
+        <button className={classes.button} onClick={increment}>BUTTON</button>
+        <About />
+
+    </div>
     );
 }
