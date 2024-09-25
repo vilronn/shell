2082356f7e7d6b1db1e764c8bb2cff9af883ef0d
vilronn <102553486+vilronn@users.noreply.github.com>
2024-09-25 08:55:43 +0300

    Add files via upload

---
+++ src/pages/shop/Shop.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Shop = () => {
+    return (
+        <div>
+        <h1>
+            SHOP
+        </h1>
+            <h2>
+            Be to her, Persephone,<br />
+            All the things I might not be;<br />
+            Take her head upon your knee.<br />
+            She that was so proud and wild,<br />
+            Flippant, arrogant and free,<br />
+            She that had no need of me,<br />
+            Is a little lonely child<br />
+            Lost in Hell,—Persephone,<br />
+            Take her head upon your knee;<br />
+            Say to her, “My dear, my dear,<br />
+            It is not so dreadful here.”
+        </h2>
+        </div>
+    );
+};
+
+export default Shop;
