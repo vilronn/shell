c4e06756bd3139142dfe1583ef910e8ce23d8e7c
vilronn <102553486+vilronn@users.noreply.github.com>
2024-09-25 08:50:28 +0300

    Update webpack.config.ts

--- webpack.config.ts
+++ webpack.config.ts
@@ -7,18 +7,21 @@ type BuildMode = 'production' | 'development';
 
 interface EnvVariables {
     mode: BuildMode;
+    analyzer?: boolean;
     port: number;
 }
 export default (env: EnvVariables) => {
     const paths: BuildPaths = {
         output: path.resolve(__dirname, 'build'),
         entry: path.resolve(__dirname, 'src', 'index.tsx'),
-        html: path.resolve(__dirname, 'public', 'index.html')
+        html: path.resolve(__dirname, 'public', 'index.html'),
+        src: path.resolve(__dirname,'src'),
     }
     const config: webpack.Configuration = buildWebpack({
         port: env.port ?? 3000,
         mode: env.mode ?? 'development',
-        paths
+        paths,
+        analyzer: env.analyzer,
     })
     return config;
   };
