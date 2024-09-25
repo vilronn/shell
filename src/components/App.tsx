3edb4f0a3e6d78cde104444d2403fae7f28e9dd1
vilronn <102553486+vilronn@users.noreply.github.com>
2024-09-25 18:00:04 +0300

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,19 +6,46 @@ import duckPng from "@/assets/duck.png";
 import fallJpg from "@/assets/fall.jpg";
 import LeaveSvg from "@/assets/leave.svg";
 
+// function TODO() {
+//     TODO2()
+// }
+
+// function TODO2() {
+//     throw new Error();
+// }
+
 export const App = () => {
     const [count, setCount] = useState<number>(0);
-    const increment = () => setCount(count + 1);
+    const increment = () => {
+        setCount(prev => prev + 1)
+        // TODO();
+    }
+
+    // if (__PLATFORM__ === 'desktop') {
+    //     return (
+    //         <div>ISDESKTOPPLATFORM
+    //         </div>
+    //     )
+    // }
+    // if (__PLATFORM__ === 'mobile') {
+    //     return (
+    //         <div>ISMOBILEPLATFORM
+    //         </div>
+    //     )
+    // }
+    // if (__ENV__ ==='development') {
+    //     //some dev stuff
+    // }
 
     return (
     <div>
+        <h1>PLATFORM={__PLATFORM__}</h1>
         <div>
             <img width={200} src={fallJpg} alt="fall" />
-            <img width={50} src={duckPng} alt="duck" />
-            
+            <img width={50} src={duckPng} alt="duck" />    
         </div>
         <div>
-            <LeaveSvg width={200} fill='red'/>
+            <LeaveSvg color={'red'} width={200}/>
         </div>
         <Link to={'/about'}>about</Link>
         <br/>
